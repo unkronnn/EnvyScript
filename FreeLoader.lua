ff440b72ebd5071c5af5a807c4b37379e9c9a448
Syukron Maulana <85090300+unkronnn@users.noreply.github.com>
2026-02-21 12:53:21 +0700

    Update loader URLs and add missing entries

--- FreeLoader.lua
+++ FreeLoader.lua
@@ -7,9 +7,9 @@ if not game:IsLoaded() then game.Loaded:Wait() end
 local loaders = {
     [6911148748] = 'https://api.luarmor.net/files/v4/loaders/4dcd7d36f19fd9c3a5c35fc8948c7f89.lua', -- CDID
     [110369730911937] = 'https://api.luarmor.net/files/v4/loaders/4dcd7d36f19fd9c3a5c35fc8948c7f89.lua', -- CDID Jawa Timur
-    [9233343468] = 'https://api.luarmor.net/files/v4/loaders/4dcd7d36f19fd9c3a5c35fc8948c7f89.lua' -- CDID Jawa Barat
-    [119987266683883] = 'https://api.luarmor.net/files/v4/loaders/ebb10dc038592bb6d9dd2de05e3cf519.lua' -- Survive LAVA for Brainrots
-    [131623223084840] = 'https://api.luarmor.net/files/v4/loaders/4dce975f61cf9ce350a1e8bf605c9700.lua' -- Escape Tsunami For Brainrots
+    [9233343468] = 'https://api.luarmor.net/files/v4/loaders/4dcd7d36f19fd9c3a5c35fc8948c7f89.lua', -- CDID Jawa Barat
+    [119987266683883] = 'https://api.luarmor.net/files/v4/loaders/ebb10dc038592bb6d9dd2de05e3cf519.lua', -- Survive LAVA for Brainrots
+    [131623223084840] = 'https://api.luarmor.net/files/v4/loaders/4dce975f61cf9ce350a1e8bf605c9700.lua', -- Escape Tsunami For Brainrots
     -- [PlaceId] =  'loader url', -- Place
 }
 
