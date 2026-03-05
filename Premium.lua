c8326af542cd2aa7411e7ef5b8229f0933787334
Syukron Maulana <85090300+unkronnn@users.noreply.github.com>
2026-03-05 18:17:52 +0700

    Update loader configuration and notification handling
    Refactor loader logic and notification system.

--- Premium.lua
+++ Premium.lua
@@ -1,28 +1,59 @@
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
+--// Setup
+local cr = cloneref or function(v) return v end
 
-repeat task.wait() until LocalPlayer and LocalPlayer.Character
-if not game:IsLoaded() then game.Loaded:Wait() end
+local Game = cr(game)
+local Pls = cr(Game:GetService("Players"))
+local UI = cr(Game:GetService("StarterGui"))
+local LP = Pls.LocalPlayer
 
-local loaders = {
-    [6911148748] = 'https://api.luarmor.net/files/v4/loaders/07a30652ebbe87970997fa6c7eef8c09.lua', -- CDID
-    [110369730911937] = 'https://api.luarmor.net/files/v4/loaders/07a30652ebbe87970997fa6c7eef8c09.lua', -- CDID Jawa Timur
-    [9233343468] = 'https://api.luarmor.net/files/v4/loaders/07a30652ebbe87970997fa6c7eef8c09.lua', -- CDID Jawa Barat
-[131378148336503] = 'https://api.luarmor.net/files/v4/loaders/f390c2859a25f13a0f0665c549915a6d.lua'
-    -- [PlaceId] =  'loader url', -- Place
+--// Init
+if not Game:IsLoaded() then Game.Loaded:Wait() end
+local Char = LP.Character or LP.CharacterAdded:Wait()
+
+--// Config
+local Config = {
+    Default = "https://api.luarmor.net/files/v4/loaders/07a30652ebbe87970997fa6c7eef8c09.lua",
+    Map = {
+        [6911148748]        = "https://api.luarmor.net/files/v4/loaders/07a30652ebbe87970997fa6c7eef8c09.lua",
+        [110369730911937]   = "https://api.luarmor.net/files/v4/loaders/07a30652ebbe87970997fa6c7eef8c09.lua",
+        [9233343468]        = "https://api.luarmor.net/files/v4/loaders/07a30652ebbe87970997fa6c7eef8c09.lua",
+        [131378148336503]   = "https://api.luarmor.net/files/v4/loaders/f390c2859a25f13a0f0665c549915a6d.lua"
+    }
 }
 
-local placeId = game.PlaceId
-local url = loaders[placeId]
+--// Utils
+local function Notify(ttl, txt)
+    pcall(function()
+        UI:SetCore("SendNotification", {
+            Title = ttl,
+            Text = txt,
+            Duration = 8
+        })
+    end)
+end
 
-if url then
-    local success, err = pcall(function()
-        local src = game:HttpGet(url)
+--// Main
+local function Loader()
+    local url = Config.Map[Game.PlaceId] or Config.Default
+    
+    Notify("Envy", "Executing")
+
+    local ok, res = pcall(function()
+        local src = Game:HttpGet(url)
         local fn = loadstring(src)
-        if fn then fn() end
+        
+        if not fn then return error("Load fail") end
+        
+        task.spawn(fn)
+        return true
     end)
 
-    if not success then LocalPlayer:Kick("Load failed: " .. tostring(err)) end
-else
-    LocalPlayer:Kick("Script not support")
+    if ok then
+        Notify("Envy", "Hello, " .. LP.DisplayName)
+    else
+        warn("Error: " .. tostring(res))
+        Notify("Envy (error)", "Check F9 Console")
+    end
 end
+
+Loader()
