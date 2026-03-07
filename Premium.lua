4377ae68f6fe5ac5b7a3efa69781bebc08cce6e2
Syukron Maulana <85090300+unkronnn@users.noreply.github.com>
2026-03-07 19:23:55 +0700

    Refactor Premium.lua for improved readability

--- Premium.lua
+++ Premium.lua
@@ -1,4 +1,4 @@
---// Setup
+-- // Setup
 local cr = cloneref or function(v) return v end
 
 local Game = cr(game)
@@ -6,44 +6,41 @@ local Pls = cr(Game:GetService("Players"))
 local UI = cr(Game:GetService("StarterGui"))
 local LP = Pls.LocalPlayer
 
---// Init
+-- // Init
 if not Game:IsLoaded() then Game.Loaded:Wait() end
 local Char = LP.Character or LP.CharacterAdded:Wait()
 
---// Config
+-- // Config
 local Config = {
     Default = "https://api.luarmor.net/files/v4/loaders/07a30652ebbe87970997fa6c7eef8c09.lua",
     Map = {
-        [6911148748]        = "https://api.luarmor.net/files/v4/loaders/07a30652ebbe87970997fa6c7eef8c09.lua",
-        [110369730911937]   = "https://api.luarmor.net/files/v4/loaders/07a30652ebbe87970997fa6c7eef8c09.lua",
-        [9233343468]        = "https://api.luarmor.net/files/v4/loaders/07a30652ebbe87970997fa6c7eef8c09.lua",
-        [131378148336503]   = "https://api.luarmor.net/files/v4/loaders/d6cf9371f4c7c2f39904bc9e03b5e5a8.lua"
+        [6911148748] = "https://api.luarmor.net/files/v4/loaders/07a30652ebbe87970997fa6c7eef8c09.lua",
+        [110369730911937] = "https://api.luarmor.net/files/v4/loaders/07a30652ebbe87970997fa6c7eef8c09.lua",
+        [9233343468] = "https://api.luarmor.net/files/v4/loaders/07a30652ebbe87970997fa6c7eef8c09.lua",
+        [131378148336503] = "https://api.luarmor.net/files/v4/loaders/d6cf9371f4c7c2f39904bc9e03b5e5a8.lua",
+        [114069860751320] = "https://api.luarmor.net/files/v4/loaders/d6cf9371f4c7c2f39904bc9e03b5e5a8.lua"
     }
 }
 
---// Utils
+-- // Utils
 local function Notify(ttl, txt)
     pcall(function()
-        UI:SetCore("SendNotification", {
-            Title = ttl,
-            Text = txt,
-            Duration = 8
-        })
+        UI:SetCore("SendNotification", {Title = ttl, Text = txt, Duration = 8})
     end)
 end
 
---// Main
+-- // Main
 local function Loader()
     local url = Config.Map[Game.PlaceId] or Config.Default
-    
+
     Notify("Envy", "Executing")
 
     local ok, res = pcall(function()
         local src = Game:HttpGet(url)
         local fn = loadstring(src)
-        
+
         if not fn then return error("Load fail") end
-        
+
         task.spawn(fn)
         return true
     end)
